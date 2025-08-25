9a242e73d196c9bdc6320eaa151fd2084663ef8e
Km2004-sptech <kaua.medeiros@sptech.school>
2025-08-25 15:02:52 -0300

    Adicinando a integral, média da integral e a escrita das informações no arquivo csv

--- Captura_Python.py
+++ Captura_Python.py
@@ -39,3 +39,26 @@ try:
         ramUsada = psutil.virtual_memory().percent
         discoTotal = round(psutil.disk_usage("/").total / (1024**3),2)
         discoUsado = psutil.disk_usage("/").percent
+
+# Guarda para cálculo da integral
+        valores_cpu.append(uso)
+        tempos.append(t)
+
+
+# Calcula integral e média (regra do trapézio)
+        if len(valores_cpu) > 0:
+            integral = round(np.trapz(valores_cpu, tempos),2)
+            media_integral = round(integral / (tempos[-1] - tempos[0]),2)
+            print(f"{timestamp} | Usuários Conectados: {usuariosLogados} | CPU: {uso}% | Ram total: {ramTotal}GB | Ram em Uso: {ramUsada}% | Disco total: {discoTotal}GB | Disco em uso: {discoUsado}% | Integral acumulada: {integral} | Média pela integral: {media_integral}%")
+
+# Grava no CSV
+        with open(ARQUIVO, "a", newline="") as f:
+            writer = csv.writer(f, delimiter="|")
+            writer.writerow([timestamp, usuariosLogados, uso, ramTotal, ramUsada, discoTotal, discoUsado, integral, media_integral])
+
+# Espera 10 segundos até próxima captura
+        time.sleep(10)
+        t += 10
+
+except KeyboardInterrupt: #(Ctrl + c)
+    print("\n=== Captura finalizada ===")
