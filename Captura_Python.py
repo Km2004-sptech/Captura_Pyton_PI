555277d9dc224a92a37b5d2b5cb890f1f6a18057
Km2004-sptech <kaua.medeiros@sptech.school>
2025-08-25 15:07:07 -0300

    Adicionando um arquivo csv separado para as informações gerais (que necessita ser capturadas apenas uma vez por servidor) e mais métricas e aprimoramentos no código

--- Captura_Python.py
+++ Captura_Python.py
@@ -5,17 +5,34 @@ import csv
 from datetime import datetime
 import numpy as np
 
-ARQUIVO = "captura_dados.csv"
+ARQUIVO = "dados_gerais.csv"
+ARQUIVO2 = "captura_dados.csv"
 
-# Cria arquivo com cabeçalho (só na primeira vez)
+# Cria arquivo com cabeçalho dos dados gerais do servidor (só na primeira vez)
 try:
     with open(ARQUIVO, "x", newline="") as f:
         writer = csv.writer(f, delimiter="|")
-        writer.writerow(["timestamp", "usuariosLogados", "cpu", "ramTotal", "ramUsada", "discoTotal", "discoUsado", "integral_acumuladada", "média_integral"])
+        writer.writerow(["nome do SO", "Realease do SO", "Versão do SO", "Processador:", "Núcleos Físicos", "Núcleos Lógicos"])
+except FileExistsError:
+    pass
+
+# Cria arquivo com cabeçalho dos dados do hardware (só na primeira vez)
+try:
+    with open(ARQUIVO2, "x", newline="") as f:
+        writer = csv.writer(f, delimiter="|")
+        writer.writerow(["timestamp", "cpu", "ramTotal", "ramUsada", "discoTotal", "discoUsado", "integral_acumuladada", "média_integral"])
 except FileExistsError:
     pass
 
 # Informações do SO
+
+nomeSo = platform.system()
+realeaseSo = platform.release()
+versaoSO = platform.version()
+processador = platform.processor()
+nucleosFisicos = psutil.cpu_count(logical=False)
+nucleosLogicos = psutil.cpu_count(logical=True)
+
 print("=== Informações do Sistema ===")
 print(f"Sistema Operacional: {platform.system()} {platform.release()}")
 print(f"Versão: {platform.version()}")
@@ -23,16 +40,18 @@ print(f"Processador: {platform.processor()}")
 print(f"Núcleos Físicos: {psutil.cpu_count(logical=False)}")
 print(f"Núcleos Lógicos (virtuais): {psutil.cpu_count(logical=True)}")
 
+with open(ARQUIVO, "a", newline="") as f:
+        writer = csv.writer(f, delimiter="|")
+        writer.writerow([nomeSo, realeaseSo, versaoSO, processador, nucleosFisicos, nucleosLogicos])
+
 valores_cpu = []
 tempos = []
 t = 0
 
 print("\n")
 print("\n=== Iniciando Captura Contínua ===")
-
 try:
     while True:
-        usuariosLogados = len(psutil.users())
         uso = psutil.cpu_percent(interval=1) # mede CPU em % (uso_k)
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         ramTotal = round(psutil.virtual_memory().total / (1024**3),2)
@@ -49,12 +68,15 @@ try:
         if len(valores_cpu) > 0:
             integral = round(np.trapz(valores_cpu, tempos),2)
             media_integral = round(integral / (tempos[-1] - tempos[0]),2)
-            print(f"{timestamp} | Usuários Conectados: {usuariosLogados} | CPU: {uso}% | Ram total: {ramTotal}GB | Ram em Uso: {ramUsada}% | Disco total: {discoTotal}GB | Disco em uso: {discoUsado}% | Integral acumulada: {integral} | Média pela integral: {media_integral}%")
+            print(f"{timestamp} | CPU: {uso}% | Ram total: {ramTotal}GB | Ram em Uso: {ramUsada}% | Disco total: {discoTotal}GB | Disco em uso: {discoUsado}% | Integral acumulada: {integral} | Média pela integral: {media_integral}%")
+
+
 
 # Grava no CSV
-        with open(ARQUIVO, "a", newline="") as f:
+
+        with open(ARQUIVO2, "a", newline="") as f:
             writer = csv.writer(f, delimiter="|")
-            writer.writerow([timestamp, usuariosLogados, uso, ramTotal, ramUsada, discoTotal, discoUsado, integral, media_integral])
+            writer.writerow([timestamp, uso, ramTotal, ramUsada, discoTotal, discoUsado, integral, media_integral])
 
 # Espera 10 segundos até próxima captura
         time.sleep(10)
