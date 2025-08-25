da186c369e36a8c2518f8c7831fcc5f81987b80d
Km2004-sptech <kaua.medeiros@sptech.school>
2025-08-25 15:00:26 -0300

    Adicionando a captura de informações do SO e Hardware

--- Captura_Python.py
+++ Captura_Python.py
@@ -14,3 +14,28 @@ try:
         writer.writerow(["timestamp", "usuariosLogados", "cpu", "ramTotal", "ramUsada", "discoTotal", "discoUsado", "integral_acumuladada", "média_integral"])
 except FileExistsError:
     pass
+
+# Informações do SO
+print("=== Informações do Sistema ===")
+print(f"Sistema Operacional: {platform.system()} {platform.release()}")
+print(f"Versão: {platform.version()}")
+print(f"Processador: {platform.processor()}")
+print(f"Núcleos Físicos: {psutil.cpu_count(logical=False)}")
+print(f"Núcleos Lógicos (virtuais): {psutil.cpu_count(logical=True)}")
+
+valores_cpu = []
+tempos = []
+t = 0
+
+print("\n")
+print("\n=== Iniciando Captura Contínua ===")
+
+try:
+    while True:
+        usuariosLogados = len(psutil.users())
+        uso = psutil.cpu_percent(interval=1) # mede CPU em % (uso_k)
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        ramTotal = round(psutil.virtual_memory().total / (1024**3),2)
+        ramUsada = psutil.virtual_memory().percent
+        discoTotal = round(psutil.disk_usage("/").total / (1024**3),2)
+        discoUsado = psutil.disk_usage("/").percent
